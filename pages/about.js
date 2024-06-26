29df37419ceb2c195ae678327516eb3501ce6c60
FarooqueAhmeed <zizopixels11@gmail.com>
2024-06-26 12:29:58 +0500

    first commit

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,14 @@
 // pages/about.js
 import Head from 'next/head'
+import { useRouter } from 'next/router';
+
 export default function About() {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push('/');
+  };
+
     return (
     
     <div>
@@ -9,6 +17,8 @@ export default function About() {
         <meta name="description" content="This is my page description about" />
       </Head>
       <h1>About Us</h1>
+      <button onClick={goToHome}>Go to Home Page</button>
+
     </div>
   ) ;
   }
